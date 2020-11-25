1d9b50e83621f618cc6ab64c963b0ed192567ef6
Vinicius Motta <viniciusmotta8@gmail.com>
2020-11-25 19:14:41 -0300

    feat: create middleware for bearer token and a route to test this

---
+++ src/middlewares/AuthMiddleware.ts
@@ -0,0 +1,40 @@
+import { Request, Response, NextFunction } from 'express'
+import jwt from 'jsonwebtoken'
+
+import envs from '../configs/envs.config'
+
+const secretMD5 = envs.SECRET_MD5!
+
+interface TokenPayload {
+  id: string
+  iat: number
+  exp: number
+}
+
+export default function AuthMiddleware (req: Request, res: Response, next: NextFunction) {
+  const { authorization } = req.headers
+
+  if (!authorization) {
+    return res.status(401).send({ message: 'No token provided' })
+  }
+
+  const parts = authorization.split(' ')
+
+  if (parts.length !== 2) {
+    return res.status(401).send({ message: 'Token error' })
+  }
+
+  const token = authorization.replace('Bearer', '').trim()
+
+  try {
+    const data = jwt.verify(token, secretMD5)
+
+    const { id } = data as TokenPayload
+
+    req.userId = id
+
+    return next()
+  } catch {
+    return res.status(401).send({ message: 'Token invalid' })
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express'
 import { createUserController } from './usecases/CreateUser'
+import AuthMiddleware from './middlewares/AuthMiddleware'
 
 const router = Router()
 
 router.post('/users/register', (req, res) => {
   return createUserController.handle(req, res)
+})
+
+router.get('/users/teste', AuthMiddleware, (req, res) => {
+  return res.json(req.userId)
 })
 
 export { router }

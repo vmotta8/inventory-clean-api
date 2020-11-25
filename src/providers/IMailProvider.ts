43e9a33a1c32e2fd92d22944b133ad9593a82702
Vinicius Motta <viniciusmotta8@gmail.com>
2020-11-25 15:12:45 -0300

    feat: create mail provider interface

---
+++ src/providers/IMailProvider.ts
@@ -0,0 +1,10 @@
+export interface IMessage {
+  to: string;
+  from: string;
+  subject: string;
+  body: string;
+}
+
+export interface IMailProvider {
+  sendMail(message: IMessage): Promise<void>;
+}

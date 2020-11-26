0dc44ba37af8558736a958f83f5690b8cf324bc6
Vinicius Motta <viniciusmotta8@gmail.com>
2020-11-26 00:54:55 -0300

    feat: add pix information controller

---
+++ src/usecases/RegisterPixInformation/RegisterPixInformationController.ts
@@ -0,0 +1,26 @@
+/* eslint-disable no-useless-constructor */
+import { Request, Response } from 'express'
+import { RegisterPixInformationUseCase } from './RegisterPixInformationUseCase'
+
+export class RegisterPixInformationController {
+  constructor (
+    private registerPixInformationUseCase: RegisterPixInformationUseCase
+  ) {}
+
+  async handle (request: Request, response: Response): Promise<Response> {
+    const { key, name, city } = request.body
+
+    try {
+      const data = await this.registerPixInformationUseCase.execute({
+        key,
+        name,
+        city,
+        userId: request.userId
+      })
+
+      return response.status(201).json(data)
+    } catch (err) {
+      return response.status(400).json({ message: err.message || 'Unexpected error.' })
+    }
+  }
+}

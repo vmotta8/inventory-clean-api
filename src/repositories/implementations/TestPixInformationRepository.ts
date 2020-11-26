e236ff1b6edfefb16c94ee5cd93e031d0e9f77e3
Vinicius Motta <viniciusmotta8@gmail.com>
2020-11-26 00:53:41 -0300

    feat: add test to pix information repository

---
+++ src/repositories/implementations/TestPixInformationRepository.ts
@@ -0,0 +1,23 @@
+import { IPixInformationRepository } from '../IPixInformationRepository'
+import { PixInformation } from '../../entities/PixInformation'
+
+export class TestPixInformationRepository implements IPixInformationRepository {
+  private AllPixInformation: PixInformation[] = [];
+
+  async findByUserId (userId: string): Promise<PixInformation> {
+    const pixInformation = this.AllPixInformation.find(information => information.userId === userId)
+
+    return pixInformation
+  }
+
+  async findByKey (key: string): Promise<PixInformation> {
+    const pixInformation = this.AllPixInformation.find(information => information.key === key)
+
+    return pixInformation
+  }
+
+  async save (pixInformation: PixInformation): Promise<void> {
+    this.AllPixInformation.push(pixInformation)
+    console.log(this.AllPixInformation)
+  }
+}

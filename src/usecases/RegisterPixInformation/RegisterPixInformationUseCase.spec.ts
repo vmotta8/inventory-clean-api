5aba74e10dfc1336dfa8cf2998b7e87a4fadb115
Vinicius Motta <viniciusmotta8@gmail.com>
2020-11-27 11:51:57 -0300

    feat: add register pix information test

---
+++ src/usecases/RegisterPixInformation/RegisterPixInformationUseCase.spec.ts
@@ -0,0 +1,45 @@
+import { TestPixInformationRepository } from '../../repositories/implementations/TestPixInformationRepository'
+import { RegisterPixInformationUseCase } from './RegisterPixInformationUseCase'
+
+const testPixInformationRepository = new TestPixInformationRepository()
+
+const registerPixInformationUseCase = new RegisterPixInformationUseCase(
+  testPixInformationRepository
+)
+
+beforeAll(async () => {
+  await registerPixInformationUseCase.execute({
+    key: '123456',
+    name: 'Vinicius',
+    city: 'São Paulo',
+    userId: '123-456'
+  })
+})
+
+describe('Create user use case', () => {
+  it('should return a error because key already exists', async () => {
+    try {
+      await registerPixInformationUseCase.execute({
+        key: '123456',
+        name: 'Vinicius2',
+        city: 'São Paulo',
+        userId: '123-4562'
+      })
+    } catch (err) {
+      expect(err.message).toBe('Key already exists.')
+    }
+  })
+
+  it('should return a error because user id already exists', async () => {
+    try {
+      await registerPixInformationUseCase.execute({
+        key: '1234562',
+        name: 'Vinicius2',
+        city: 'São Paulo',
+        userId: '123-456'
+      })
+    } catch (err) {
+      expect(err.message).toBe('User id already exists.')
+    }
+  })
+})

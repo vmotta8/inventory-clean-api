199fffbddef6d030deb1cf4e6ca733d9bdab5092
Vinicius Motta <viniciusmotta8@gmail.com>
2020-11-30 10:48:11 -0300

    refactor: move email validator to entity

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,5 +1,6 @@
 import { uuidHelper } from './helpers/uuidHelper'
 import { bcryptHelper } from './helpers/bcryptHelper'
+import { emailValidator } from './validators/emailValidator'
 
 export class User {
   public readonly id: string;
@@ -9,9 +10,16 @@ export class User {
   public password: string;
 
   constructor (props: Omit<User, 'id'>) {
-    this.id = uuidHelper.create()
-    this.password = bcryptHelper.generateHash(props.password)
     this.name = props.name
-    this.email = props.email
+
+    this.id = uuidHelper.create()
+
+    this.password = bcryptHelper.generateHash(props.password)
+
+    if (emailValidator(props.email)) {
+      this.email = props.email
+    } else {
+      throw new Error('Invalid email.')
+    }
   }
 }
--- src/usecases/CreateUser/CreateUserUseCase.ts
+++ src/usecases/CreateUser/CreateUserUseCase.ts
@@ -13,7 +13,6 @@ export class CreateUserUseCase {
 
   async execute (data: ICreateUserRequestDTO) {
     const user = new User(data)
-    console.log(user)
 
     const userAlreadyExists = await this.usersRepository.findByEmail(data.email)
     if (userAlreadyExists) {
@@ -22,11 +21,6 @@ export class CreateUserUseCase {
 
     if ((data.name).length < 1) {
       throw new Error('Invalid name.')
-    }
-
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    if (!re.test(data.email)) {
-      throw new Error('Invalid email.')
     }
 
     if ((data.password).length !== ((data.password).replace(/\s/g, '')).length || (data.password).length < 6) {

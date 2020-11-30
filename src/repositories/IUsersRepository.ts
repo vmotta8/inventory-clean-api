f9fbc74c2e91d3d4c39f1a63143c154ffb41422f
Vinicius Motta <viniciusmotta8@gmail.com>
2020-11-30 11:18:05 -0300

    feat: create find user by id

--- src/repositories/IUsersRepository.ts
+++ src/repositories/IUsersRepository.ts
@@ -2,5 +2,6 @@ import { User } from '../entities/User'
 
 export interface IUsersRepository {
   findByEmail (email: string): Promise<User>;
+  findById (id: string): Promise<User>;
   save (user: User): Promise<void>;
 }
--- src/repositories/implementations/TestUsersRepository.ts
+++ src/repositories/implementations/TestUsersRepository.ts
@@ -10,6 +10,12 @@ export class TestUsersRepository implements IUsersRepository {
     return user
   }
 
+  async findById (id: string): Promise<User> {
+    const user = this.users.find(user => user.id === id)
+
+    return user
+  }
+
   async save (user: User): Promise<void> {
     this.users.push(user)
   }

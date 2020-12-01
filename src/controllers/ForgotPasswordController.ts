3b177af42eb6fbc7184e6d9100e3b80d0549a5a0
Vinicius Motta <viniciusmotta8@gmail.com>
2020-11-30 23:35:41 -0300

    feat: add forgot password controller

---
+++ src/controllers/ForgotPasswordController.ts
@@ -0,0 +1,23 @@
+/* eslint-disable no-useless-constructor */
+import { Request, Response } from 'express'
+import { ForgotPasswordUseCase } from '../usecases/ForgotPassword/ForgotPasswordUseCase'
+
+export class ForgotPasswordController {
+  constructor (
+    private forgotPasswordUseCase: ForgotPasswordUseCase
+  ) {}
+
+  async handle (request: Request, response: Response): Promise<Response> {
+    const { email } = request.body
+
+    try {
+      const data = await this.forgotPasswordUseCase.execute({
+        email
+      })
+
+      return response.status(201).json(data)
+    } catch (err) {
+      return response.status(400).json({ message: err.message || 'Unexpected error.' })
+    }
+  }
+}

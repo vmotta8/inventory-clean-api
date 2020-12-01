68219c906781f541ad9bb2b000c0b320d23fb4dd
Vinicius Motta <viniciusmotta8@gmail.com>
2020-12-01 03:45:28 -0300

    feat: create mongo helper

--- src/configs/envs.config.ts
+++ src/configs/envs.config.ts
@@ -2,5 +2,6 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export default {
-  SECRET_MD5: process.env.SECRET_MD5
+  SECRET_MD5: process.env.SECRET_MD5,
+  MONGO_URL: process.env.MONGO_URL
 }
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,7 @@
+import { MongoHelper } from './database'
 import { app } from './app'
+import envs from './configs/envs.config'
+
+MongoHelper.connect(envs.MONGO_URL)
 
 app.listen(3333)

f0fb0ae5ab7c655f1aec9efb0f1ce78f510b235f
Vinicius Motta <viniciusmotta8@gmail.com>
2020-12-02 23:28:14 -0300

    test: add more tests

--- src/entities/validators/__tests__/emailValidator.spec.ts
+++ src/entities/validators/__tests__/emailValidator.spec.ts
@@ -1,8 +1,32 @@
 import { emailValidator } from '../emailValidator'
 
 describe('Email validator', () => {
-  it('should return a error because email is invalid', () => {
+  it('should a valid email', () => {
+    const email = 'vinicius@email.com'
+
+    const valid = emailValidator(email)
+
+    expect(valid).toBe(true)
+  })
+
+  it('should return a error because email is invalid 1', () => {
     const email = 'vinicius@emailcom'
+
+    const valid = emailValidator(email)
+
+    expect(valid).toBe(false)
+  })
+
+  it('should return a error because email is invalid 2', () => {
+    const email = 'viniciusemail.com'
+
+    const valid = emailValidator(email)
+
+    expect(valid).toBe(false)
+  })
+
+  it('should return a error because email is invalid 3', () => {
+    const email = 'vinicius'
 
     const valid = emailValidator(email)
 

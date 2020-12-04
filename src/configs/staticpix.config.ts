5d006a70d61c418c13e780c78bf47dbe2955031d
Vinicius Motta <viniciusmotta8@gmail.com>
2020-12-04 11:24:16 -0300

    feat: add static pix config

---
+++ src/configs/staticpix.config.ts
@@ -0,0 +1,8 @@
+export default {
+  PayloadFormatIndicator: '000201',
+  PointofInitiationMethod: '010212',
+  MerchantAccountInformation: '0014br.gov.bcb.pix',
+  MerchantCategoryCode: '52040000',
+  TransactionCurrency: '5303986',
+  CountryCode: '5802BR'
+}

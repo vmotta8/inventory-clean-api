583a7ef78b1648532395e172033b041c0ea106bd
Vinicius Motta <viniciusmotta8@gmail.com>
2020-12-04 10:23:57 -0300

    test: add crc16 helper spec

---
+++ src/helpers/__tests__/crc16Helper.spec.ts
@@ -0,0 +1,10 @@
+import { crc16Helper } from '../crc16Helper'
+
+describe('crc16 helper', () => {
+  it('should return a string and right value', () => {
+    const data = '00020101021126440014br.gov.bcb.spi0122fulano2019@example.com5204000053039865802BR5913FULANO DE TAL6008BRASILIA6304'
+    const crc = crc16Helper.generate(data)
+    expect(typeof crc).toBe('string')
+    expect(crc).toBe('DFE3')
+  })
+})

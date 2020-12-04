fcc803060e421896a0810c85c91d1c2495519013
Vinicius Motta <viniciusmotta8@gmail.com>
2020-12-04 11:30:41 -0300

    feat: add generate pix index

---
+++ src/usecases/GeneratePixString/index.ts
@@ -0,0 +1,15 @@
+import { MongoPixInformationRepository } from '../../repositories/implementations/MongoPixInformationRepository'
+import { GeneratePixStringUseCase } from './GeneratePixStringUseCase'
+import { GeneratePixStringController } from '../../controllers/GeneratePixStringController'
+
+const mongoPixInformationRepository = new MongoPixInformationRepository()
+
+const generatePixStringUseCase = new GeneratePixStringUseCase(
+  mongoPixInformationRepository
+)
+
+const generatePixStringController = new GeneratePixStringController(
+  generatePixStringUseCase
+)
+
+export { generatePixStringController }

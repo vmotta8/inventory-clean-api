b44307e188a9233ced6a531f67818e1232e5f637
Vinicius Motta <viniciusmotta8@gmail.com>
2020-12-04 11:31:01 -0300

    feat: add generate pix to routes

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import AuthMiddleware from './middlewares/AuthMiddleware'
 import { createUserController } from './usecases/CreateUser'
 import { registerPixInformationController } from './usecases/RegisterPixInformation'
 import { forgotPasswordController } from './usecases/ForgotPassword'
+import { generatePixStringController } from './usecases/GeneratePixString'
 
 const router = Router()
 
@@ -10,12 +11,16 @@ router.post('/users/register', (req, res) => {
   return createUserController.handle(req, res)
 })
 
+router.post('/users/forgot_password', (req, res) => {
+  return forgotPasswordController.handle(req, res)
+})
+
 router.post('/pix/register', AuthMiddleware, (req, res) => {
   return registerPixInformationController.handle(req, res)
 })
 
-router.post('/users/forgot_password', (req, res) => {
-  return forgotPasswordController.handle(req, res)
+router.get('/pix/generate_string', AuthMiddleware, (req, res) => {
+  return generatePixStringController.handle(req, res)
 })
 
 export { router }

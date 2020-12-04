daf0c02bcc45818a569c92f289c5c23e43336eb6
Vinicius Motta <viniciusmotta8@gmail.com>
2020-12-04 11:27:14 -0300

    feat: add generate pix controller

---
+++ src/controllers/GeneratePixStringController.ts
@@ -0,0 +1,24 @@
+/* eslint-disable no-useless-constructor */
+import { Request, Response } from 'express'
+import { GeneratePixStringUseCase } from '../usecases/GeneratePixString/GeneratePixStringUseCase'
+
+export class GeneratePixStringController {
+  constructor (
+    private generatePixStringUseCase: GeneratePixStringUseCase
+  ) {}
+
+  async handle (request: Request, response: Response): Promise<Response> {
+    const { value } = request.body
+
+    try {
+      const data = await this.generatePixStringUseCase.execute({
+        value,
+        userId: request.userId
+      })
+
+      return response.status(201).json(data)
+    } catch (err) {
+      return response.status(400).json({ message: err.message || 'Unexpected error.' })
+    }
+  }
+}

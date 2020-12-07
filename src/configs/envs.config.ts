3c6c7785d8ecfc7ad74bf53ff4f930cdb22d5732
Vinicius Motta <viniciusmotta8@gmail.com>
2020-12-07 15:22:22 -0300

    feat: add aws and email

--- src/configs/envs.config.ts
+++ src/configs/envs.config.ts
@@ -3,5 +3,8 @@ dotenv.config()
 
 export default {
   SECRET_MD5: process.env.SECRET_MD5,
-  MONGO_URL: process.env.MONGO_URL
+  MONGO_URL: process.env.MONGO_URL,
+  AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+  EMAIL: process.env.EMAIL
 }

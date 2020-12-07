9c46be8bf7c2ff6ea87351ba32fc7d22df4b51e9
Vinicius Motta <viniciusmotta8@gmail.com>
2020-12-07 14:44:02 -0300

    feat: add compare to bcrypt helper

--- src/helpers/bcryptHelper.ts
+++ src/helpers/bcryptHelper.ts
@@ -5,5 +5,11 @@ export const bcryptHelper = {
     const hash = bcrypt.hashSync(password, 10)
 
     return hash
+  },
+
+  async compare (password: string, hash: string): Promise<boolean> {
+    const valid = await bcrypt.compare(password, hash)
+
+    return valid
   }
 }

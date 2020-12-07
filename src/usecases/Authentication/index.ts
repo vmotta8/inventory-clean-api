fe2eceb9df5a631baf01f4db45cfa8dc0c41c11b
Vinicius Motta <viniciusmotta8@gmail.com>
2020-12-07 14:45:04 -0300

    feat: add authentication index

---
+++ src/usecases/Authentication/index.ts
@@ -0,0 +1,15 @@
+import { MongoUsersRepository } from '../../repositories/implementations/MongoUsersRepository'
+import { AuthenticationUseCase } from './AuthenticationUseCase'
+import { AuthenticationController } from '../../controllers/AuthenticationController'
+
+const mongoUsersRepository = new MongoUsersRepository()
+
+const authenticationUseCase = new AuthenticationUseCase(
+  mongoUsersRepository
+)
+
+const authenticationController = new AuthenticationController(
+  authenticationUseCase
+)
+
+export { authenticationController }

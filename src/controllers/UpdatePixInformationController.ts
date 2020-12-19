e2bd8af580a91840ee3127ed488802b1f1857e4a
Vinicius Motta <viniciusmotta8@gmail.com>
2020-12-18 22:39:50 -0300

    feat: add update pix information controller

---
+++ src/controllers/UpdatePixInformationController.ts
@@ -0,0 +1,26 @@
+/* eslint-disable no-useless-constructor */
+import { Request, Response } from 'express'
+import { UpdatePixInformationUseCase } from '../usecases/UpdatePixInformation/UpdatePixInformationUseCase'
+
+export class UpdatePixInformationController {
+  constructor (
+    private updatePixInformationUseCase: UpdatePixInformationUseCase
+  ) {}
+
+  async handle (request: Request, response: Response): Promise<Response> {
+    const { key, name, city } = request.body
+
+    try {
+      await this.updatePixInformationUseCase.execute({
+        key,
+        name,
+        city,
+        userId: request.userId
+      })
+
+      return response.status(201).send()
+    } catch (err) {
+      return response.status(400).json({ message: err.message || 'Unexpected error.' })
+    }
+  }
+}

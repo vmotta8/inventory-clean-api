10143400afc58bb3fda05cd7f0e5734de348112e
Vinicius Motta <viniciusmotta8@gmail.com>
2020-12-20 18:31:46 -0300

    refactor: improve name validator

--- src/entities/validators/nameValidator.ts
+++ src/entities/validators/nameValidator.ts
@@ -1,6 +1,11 @@
 export function nameValidator (name: string): boolean {
-  if ((name).length < 2) {
+  if (!name) {
     return false
   }
+
+  if (name.trim().length < 2 || name.trim().length > 256) {
+    return false
+  }
+
   return true
 }

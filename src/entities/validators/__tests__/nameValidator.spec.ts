2d0a52a13a7838a28c66e025fc331d59ab19dcfc
Vinicius Motta <viniciusmotta8@gmail.com>
2020-12-20 18:32:02 -0300

    test: improve name validator

--- src/entities/validators/__tests__/nameValidator.spec.ts
+++ src/entities/validators/__tests__/nameValidator.spec.ts
@@ -1,11 +1,20 @@
 import { nameValidator } from '../nameValidator'
 
 describe('Name validator', () => {
-  it('should return a error because name is invalid', () => {
+  it('should return a error because name is invalid empty', () => {
     const name = ''
-
     const valid = nameValidator(name)
+    expect(valid).toBe(false)
+  })
+  it('should not create user with invalid name too few', () => {
+    const name = 'O     '
+    const valid = nameValidator(name)
+    expect(valid).toBe(false)
+  })
 
+  it('should not create user with invalid name too many', () => {
+    const name = 'O'.repeat(257)
+    const valid = nameValidator(name)
     expect(valid).toBe(false)
   })
 })

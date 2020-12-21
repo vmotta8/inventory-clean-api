368b73130373f432368258aec9333a7f7fe5fa52
Vinicius Motta <viniciusmotta8@gmail.com>
2020-12-21 19:20:10 -0300

    style: remove spaces

--- src/entities/validators/__tests__/passwordValidator.spec.ts
+++ src/entities/validators/__tests__/passwordValidator.spec.ts
@@ -3,25 +3,19 @@ import { passwordValidator } from '../passwordValidator'
 describe('Password validator', () => {
   it('should return true for a valid password', () => {
     const password = '123456'
-
     const valid = passwordValidator(password)
-
     expect(valid).toBe(true)
   })
 
-  it('should return an error because password is invalid 1', () => {
+  it('should return an error because password is invalid', () => {
     const password = '123 456'
-
     const valid = passwordValidator(password)
-
     expect(valid).toBe(false)
   })
 
   it('should return an error because the password is less than 6 digits', () => {
     const password = '12345'
-
     const valid = passwordValidator(password)
-
     expect(valid).toBe(false)
   })
 })

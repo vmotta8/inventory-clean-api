4a5aaaea22fbe5188dc7450feed813087a8f7537
Vinicius Motta <viniciusmotta8@gmail.com>
2021-01-05 19:47:55 -0300

    test: add register pix information use case test

---
+++ src/usecases/RegisterPixInformation/RegisterPixInformationUseCase.spec.ts
@@ -0,0 +1,47 @@
+import { MongoHelper } from '../../database'
+import { registerPixInformationUseCase } from './index'
+import envs from '../../configs/envs.config'
+
+beforeAll(async () => {
+  await MongoHelper.connect(envs.MONGO_URL_TEST)
+  MongoHelper.clearCollection('pixinformation')
+
+  await registerPixInformationUseCase.execute({
+    name: 'Vinicius Motta',
+    key: 'viniciusmotta8@gmail.com',
+    city: 'Mogi Mirim',
+    userId: '123456'
+  })
+})
+
+afterAll(async () => {
+  await MongoHelper.disconnect()
+})
+
+describe('register pix information use case', () => {
+  it('key already exists', async () => {
+    try {
+      await registerPixInformationUseCase.execute({
+        name: 'Vinicius Motta',
+        key: 'viniciusmotta8@gmail.com',
+        city: 'Mogi Mirim',
+        userId: '12345678'
+      })
+    } catch (error) {
+      expect(error.message).toEqual('Key already exists.')
+    }
+  })
+
+  it('userId already exists', async () => {
+    try {
+      await registerPixInformationUseCase.execute({
+        name: 'Vinicius Motta',
+        key: 'viniciusmotta8@email.com',
+        city: 'Mogi Mirim',
+        userId: '123456'
+      })
+    } catch (error) {
+      expect(error.message).toEqual('User id already exists.')
+    }
+  })
+})

ab38f2203c7a939cbc95f3ede33e4b7d35da1dc5
Vinicius Motta <viniciusmotta8@gmail.com>
2021-01-25 12:26:39 -0300

    style: add mensage when database starts

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,6 @@ import { database } from '@/database/index'
 import { app } from '@/app'
 import envs from '@/configs/envs.config'
 
-database.connect(envs.MONGO_URL)
+database.connect(envs.MONGO_URL).then(() => console.log('Database connected.'))
 
 app.listen(3333)
